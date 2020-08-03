d8df1c18de5605a4a139c9372391989466b8393a
JohnMMcD <jmcdonne@gmail.com>
2020-08-02 21:30:10 -0400

    Updated to use buttons. Very simple, very easy.

--- TeamCode/src/org/firstinspires/ftc/teamcode/RangerTankDrive.java
+++ TeamCode/src/org/firstinspires/ftc/teamcode/RangerTankDrive.java
@@ -3,14 +3,13 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-
 import virtual_robot.controller.VirtualRobotController;
 
 /**
  * Example OpMode. Controls robot using left joystick, with arcade drive.
  */
 
-@TeleOp(name = "RangerTankDrive", group = "TwoWheel")
+@TeleOp(name = "_RangerTankDrive", group = "")
 public class RangerTankDrive extends LinearOpMode {
     private VirtualRobotController.ColorSensorImpl colorSensor = null;
 
@@ -36,18 +35,24 @@ public class RangerTankDrive extends LinearOpMode {
                 leftPower = -1.0;
                 rightPower = -1.0;
             }
-            else if (Math.abs(gamepad1.left_stick_y) < 0.05 || Math.abs(gamepad1.left_stick_x) < 0.05)
+            else if (gamepad1.x)
+            {
+                leftPower = 0.5;
+                rightPower = -0.5;
+            }
+            else if (gamepad1.b)
+            {
+                leftPower = -0.5;
+                rightPower = 0.5;
+            }
+            else
             {
                 leftPower = 0.0;
                 rightPower = 0.0;
             }
-            else
-            {
-                leftPower = -gamepad1.left_stick_y;
-                rightPower = gamepad1.left_stick_x;
-            }
             left.setPower(leftPower);
             right.setPower(rightPower);
+            //TODO: telemetry.addData("X and Y", String.format("X:%.2f Y:%.2f", 0.0, 0.0 ));
             telemetry.addData("Power", String.format("L: %.2f R: %.2f", leftPower, rightPower));
             telemetry.addData("Color", String.format("R:%2d G: %2d B: %2d", colorSensor.red(), colorSensor.green(), colorSensor.blue()));
             telemetry.update();
